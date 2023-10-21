e486b5996e66987b4b010dfc843222044fd5ac62
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-10-21 10:47:54 -0300

    Scaffolding do Microservice de Clientes

---
+++ msclientes/src/main/java/io/github/eduardoconceicao90/msclientes/MsclientesApplication.java
@@ -0,0 +1,13 @@
+package io.github.eduardoconceicao90.msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclientesApplication.class, args);
+	}
+
+}
